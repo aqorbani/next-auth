c0bc04a727704820d623e200a8e3d42dea38a055
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-20 00:39:59 +0330

    install mongoose and adding the hash and verify password

---
+++ utils/auth.js
@@ -0,0 +1,13 @@
+const { hash, compare } = require("bcrypt");
+
+async function hashPassword(password) {
+  const hashedPassword = await hash(password, 12);
+  return hashedPassword;
+}
+
+async function verifyPassword(password, hashedPassword) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+}
+
+export { hashPassword, verifyPassword };
