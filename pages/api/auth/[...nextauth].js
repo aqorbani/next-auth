046326f4d3e41e47ea82be26490d293999fba764
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-20 20:53:15 +0330

    install next-auth labrary and create nextauth default file

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your email",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials, req) {
+        return { name: "amir" };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
