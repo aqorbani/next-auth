f75ec71d42aa9e707e2e19f0bc30b160be3dd945
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-20 22:01:16 +0330

    Implementation login cycle

--- pages/signin.js
+++ pages/signin.js
@@ -1,3 +1,42 @@
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 export default function SignIn() {
-  return <div>signin</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const signInHandler = async (e) => {
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    if (!res.error) {
+      router.push("/dashboard");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login Form</h2>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={signInHandler}>Sign In</button>
+    </div>
+  );
 }
