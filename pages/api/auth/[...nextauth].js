904ada6e4320aab9bb70b6aab03d06a3c3507ec5
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-20 21:45:33 +0330

    Implementation authorize function

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,6 @@
+import User from "@/models/User";
+import connectDB from "@/utils/ConnectDB";
+import { verifyPassword } from "@/utils/auth";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -5,27 +8,36 @@ export const authOptions = {
   session: { strategy: "jwt" },
   providers: [
     CredentialsProvider({
-      name: "login",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "Enter your email",
-        },
-        password: {
-          label: "password",
-          type: "password",
-          placeholder: "Enter your password",
-        },
-      },
       async authorize(credentials, req) {
-        return { name: "amir" };
+        const { email, password } = credentials;
+
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("Error in connecting to DB!");
+        }
+
+        if (email && password) {
+          const existingUser = await User.findOne({ email: email });
+          if (existingUser) {
+            const isValid = await verifyPassword(
+              password,
+              existingUser.password
+            );
+            if (isValid) {
+              return { email };
+            } else {
+              throw new Error("Username or Password is incorrect!");
+            }
+          } else {
+            throw new Error("User doesn't exist!");
+          }
+        } else {
+          throw new Error("Invalid data!");
+        }
       },
     }),
   ],
-  pages: {
-    signIn: "/signin",
-  },
 };
 
 export default NextAuth(authOptions);
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>hello</h1>
-      <button onClick={() => signIn()}>Sign in</button>
+      <button>
+        <Link href="/signup">Sign Up</Link>
+      </button>
+      <button>
+        <Link href="/signin">Sign In</Link>
+      </button>
     </>
   );
 }
