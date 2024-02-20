d6eb7d59e87cfbcbcc519056ed637423968558eb
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-21 00:00:17 +0330

    getSession method

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,20 @@
+import { getSession } from "next-auth/react";
+
 export default function dashboard() {
   return <div>dashboard</div>;
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
 }
